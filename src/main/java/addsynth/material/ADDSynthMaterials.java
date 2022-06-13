c528550621d246f684a66b9c0d191778d5863fc0
ADDSynth <addsynth@gmail.com>
2022-06-13 13:35:17 -0500

    Warn if this is a Development Build

--- src/main/java/addsynth/material/ADDSynthMaterials.java
+++ src/main/java/addsynth/material/ADDSynthMaterials.java
@@ -75,7 +75,7 @@ public final class ADDSynthMaterials {
 
   private static final void main_setup(final FMLCommonSetupEvent event){
     // log.info("Begin ADDSynthMaterials main setup...");
-    log.info(CommonUtil.get_mod_info(MOD_NAME, "ADDSynth", VERSION, DevStage.STABLE, VERSION_DATE));
+    CommonUtil.displayModInfo(log, MOD_NAME, "ADDSynth", VERSION, DevStage.STABLE, VERSION_DATE);
     // DELETE: OreGenerator.register();
     // event.enqueueWork(OreGenerator::register);
     // log.info("Finished ADDSynthMaterials main setup.");
