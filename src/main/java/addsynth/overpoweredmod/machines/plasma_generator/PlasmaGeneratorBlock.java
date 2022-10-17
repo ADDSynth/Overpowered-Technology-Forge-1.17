111020694c56790e96ecc22550cb9e8a00b788c0
ADDSynth <addsynth@gmail.com>
2022-10-17 10:49:18 -0500

    Updated Plasma Generator Texture

--- src/main/java/addsynth/overpoweredmod/machines/plasma_generator/PlasmaGeneratorBlock.java
+++ src/main/java/addsynth/overpoweredmod/machines/plasma_generator/PlasmaGeneratorBlock.java
@@ -9,6 +9,7 @@ import addsynth.overpoweredmod.assets.CreativeTabs;
 import addsynth.overpoweredmod.game.reference.Names;
 import addsynth.overpoweredmod.registers.Tiles;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.level.ServerPlayer;
@@ -17,21 +18,29 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.MaterialColor;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.fmllegacy.network.NetworkHooks;
 
 public final class PlasmaGeneratorBlock extends MachineBlock {
 
+  public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+
   public PlasmaGeneratorBlock(){
     super(MaterialColor.WOOL);
     RegistryUtil.register_block(this, Names.PLASMA_GENERATOR, CreativeTabs.creative_tab);
+    this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
   }
 
   @Override
@@ -61,6 +70,17 @@ public final class PlasmaGeneratorBlock extends MachineBlock {
       }
     }
     return InteractionResult.SUCCESS;
+  }
+
+  @Override
+  @Nullable
+  public BlockState getStateForPlacement(BlockPlaceContext context){
+    return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+  }
+
+  @Override
+  protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder){
+    builder.add(FACING);
   }
 
 }
