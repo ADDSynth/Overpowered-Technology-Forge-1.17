0962d43b29dbc4e6b7b88e0ffd59ce3ce3b8fae4
ADDSynth <addsynth@gmail.com>
2023-09-29 15:24:39 -0500

    Remove Unused Variant of EnergyUtil.balance_batteries
    Unused since the BlockNetwork update.

--- src/main/java/addsynth/energy/lib/energy_network/EnergyUtil.java
+++ src/main/java/addsynth/energy/lib/energy_network/EnergyUtil.java
@@ -108,16 +108,12 @@ public final class EnergyUtil {
   }
 
   public static final void balance_batteries(final ArrayList<EnergyNode> batteries){
-    balance_batteries(batteries, 0);
-  }
-  
-  public static final void balance_batteries(final ArrayList<EnergyNode> batteries, final long removed_battery_energy){
 
     int i;
     final int length = batteries.size();
     Energy energy_storage;
 
-    long total_energy = removed_battery_energy;
+    long total_energy = 0;
     long total_capacity = 0;
     final long[] capacity = new long[length];
 
